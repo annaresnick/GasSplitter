20bee8ea4ec4c12ffca978d90a72e50b4dffc5d4
Anna Resnick <annaresnick@annas-macbook-pro.local>
2017-05-23 15:16:50 -0700

    Initial Commit

---
+++ GasSplitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GasSplitter
+//
+//  Created by Anna Resnick on 5/23/17.
+//  Copyright © 2017 AnnaResnick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
