6f8ab0ef1364cd385f62e95a977a6fb4b70f0a49
Anna Resnick <annaresnick@Annas-MacBook-Pro.local>
2017-06-04 20:36:02 -0700

    Api gone

--- GasSplitter/AppDelegate.swift
+++ GasSplitter/AppDelegate.swift
@@ -52,7 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        GMSPlacesClient.provideAPIKey("AIzaSyAMNEesjm13sGWJwBxArTb-xYDX682126Q")
+        
 
     }
 
